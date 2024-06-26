f2ccbe7c8ddf91bd114ec9010adf3606d62bee0b
Jaimul Islam Akik <jaimulislam7@gmail.com>
2024-06-26 20:50:50 +0600

    Add files via upload

---
+++ practice.dart
@@ -0,0 +1,149 @@
+void main() {
+
+  print('-------------------------');
+  print('multiplication table of 1');
+  print('-------------------------');
+
+  print('1 * 1 = 1');
+  print('1 * 2 = 2');
+  print('1 * 3 = 3');
+  print('1 * 4 = 4');
+  print('1 * 5 = 5');
+  print('1 * 6 = 6');
+  print('1 * 7 = 7');
+  print('1 * 8 = 8');
+  print('1 * 9 = 9');
+  print('1 * 10 = 10');
+
+  print('-------------------------');
+  print('multiplication table of 2');
+  print('-------------------------');
+  print('2 * 1 = 2');
+  print('2 * 2 = 4');
+  print('2 * 3 = 6');
+  print('2 * 4 = 8');
+  print('2 * 5 = 10');
+  print('2 * 6 = 12');
+  print('2 * 7 = 14');
+  print('2 * 8 = 16');
+  print('2 * 9 = 18');
+  print('2 * 10 = 20');
+
+  print('-------------------------');
+  print('multiplication table of 3');
+  print('-------------------------');
+  print('3 * 1 = 3');
+  print('3 * 2 = 6');
+  print('3 * 3 = 9');
+  print('3 * 4 = 12');
+  print('3 * 5 = 15');
+  print('3 * 6 = 18');
+  print('3 * 7 = 21');
+  print('3 * 8 = 24');
+  print('3 * 9 = 27');
+  print('3 * 10 = 30');
+
+  print('-------------------------');
+  print('multiplication table of 4');
+  print('-------------------------');
+
+  print('4 * 1 = 4');
+  print('4 * 2 = 8');
+  print('4 * 3 = 12');
+  print('4 * 4 = 16');
+  print('4 * 5 = 20');
+  print('4 * 6 = 24');
+  print('4 * 7 = 28');
+  print('4 * 8 = 32');
+  print('4 * 9 = 36');
+  print('4 * 10 = 40');
+
+  print('-------------------------');
+  print('multiplication table of 5');
+  print('-------------------------');
+
+  print('5 * 1 = 5');
+  print('5 * 2 = 10');
+  print('5 * 3 = 15');
+  print('5 * 4 = 20');
+  print('5 * 5 = 25');
+  print('5 * 6 = 30');
+  print('5 * 7 = 35');
+  print('5 * 8 = 40');
+  print('5 * 9 = 45');
+  print('5 * 10 = 50');
+  print('-------------------------');
+  print('multiplication table of 6');
+  print('-------------------------');
+
+  print('6 * 1 = 6');
+  print('6 * 2 = 12');
+  print('6 * 3 = 18');
+  print('6 * 4 = 24');
+  print('6 * 5 = 30');
+  print('6 * 6 = 36');
+  print('6 * 7 = 42');
+  print('6 * 8 = 48');
+  print('6 * 9 = 54');
+  print('6 * 10 = 60');
+
+  print('-------------------------');
+  print('multiplication table of 7');
+  print('-------------------------');
+
+  print('7 * 1 = 7');
+  print('7 * 2 = 14');
+  print('7 * 3 = 21');
+  print('7 * 4 = 28');
+  print('7 * 5 = 35');
+  print('7 * 6 = 42');
+  print('7 * 7 = 49');
+  print('7 * 8 = 56');
+  print('7 * 9 = 63');
+  print('7 * 10 = 70');
+  print('-------------------------');
+  print('multiplication table of 8');
+  print('-------------------------');
+
+  print('8 * 1 = 8');
+  print('8 * 2 = 16');
+  print('8 * 3 = 24');
+  print('8 * 4 = 32');
+  print('8 * 5 = 40');
+  print('8 * 6 = 48');
+  print('8 * 7 = 56');
+  print('8 * 8 = 64');
+  print('8 * 9 = 72');
+  print('8 * 10 = 80');
+
+  print('-------------------------');
+  print('multiplication table of 9');
+  print('-------------------------');
+
+  print('9 * 1 = 9');
+  print('9 * 2 = 18');
+  print('9 * 3 = 27');
+  print('9 * 4 = 36');
+  print('9 * 5 = 45');
+  print('9 * 6 = 54');
+  print('9 * 7 = 63');
+  print('9 * 8 = 72');
+  print('9 * 9 = 81');
+  print('9 * 10 = 90');
+
+  print('-------------------------');
+  print('multiplication table of 10');
+  print('-------------------------');
+
+  print('10 * 1 = 10');
+  print('10 * 2 = 20');
+  print('10 * 3 = 30');
+  print('10 * 4 = 40');
+  print('10 * 5 = 50');
+  print('10 * 6 = 60');
+  print('10 * 7 = 70');
+  print('10 * 8 = 80');
+  print('10 * 9 = 90');
+  print('10 * 10 = 100');
+
+}
