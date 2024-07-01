9514d5a4a5069d4dbab09661c15bd9c6f9af1c4e
Jaimul Islam Akik <105303801+devjaimul@users.noreply.github.com>
2024-07-01 21:55:51 +0600

    updated

--- gradeSystem.dart
+++ gradeSystem.dart
@@ -1,24 +1,34 @@
 import 'dart:io';
 
-void main() {
+void main(){
   print("Enter a grade percentage (0-100): ");
-  int grade = int.parse(stdin.readLineSync()!);
+  int grade=int.parse(stdin.readLineSync()!);
 
-  if (grade < 0 || grade > 100) {
-    print("Invalid grade. Please enter a value between 0 and 100.");
-  } else {
-    String letterGrade;
-    if (grade >= 90) {
-      letterGrade = "A";
-    } else if (grade >= 80) {
-      letterGrade = "B";
-    } else if (grade >= 70) {
-      letterGrade = "C";
-    } else if (grade >= 60) {
-      letterGrade = "D";
-    } else {
-      letterGrade = "F";
+  if(grade>=100 || grade<=0){
+    print('Invalid grade. Please enter a value between 0 and 100.');
+  }
+  else{
+    if(grade>=80){
+      print('you have got A+');
     }
-    print("Your letter grade is: $letterGrade");
+    else if(grade>=70){
+      print('you have got A');
+    }
+    else if(grade>=60){
+      print('you have got B+');
+    }
+    else if(grade>=50){
+      print('you have got C+');
+    }
+    else if(grade>=40){
+      print('you have got D+');
+    }
+    else if(grade>=33){
+      print('you have got E+');
+    }
+    else if(grade<33){
+      print('you have got F+');
+    }
+
   }
 }
