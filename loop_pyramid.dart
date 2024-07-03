1122134ba278db0917a0e3afb6105c6d46da3da3
Jaimul Islam Akik <105303801+devjaimul@users.noreply.github.com>
2024-07-03 22:24:52 +0600

    updated

--- loop_pyramid.dart
+++ loop_pyramid.dart
@@ -1,9 +1,13 @@
 import 'dart:io';
 
 void main(){
-  for (var i=1;i<=5;i++){
-    for(var j=1;j<=i;j++){
-      stdout.write("# ");
+  var num=5;
+  for (var i=1;i<=num;i++){
+    for(var j=1;j<=num-i;j++){
+      stdout.write(" ");
+    }
+    for(var k=1;k<=(2*i-1);k++){
+      stdout.write("1");
     }
     stdout.writeln('');
   }
